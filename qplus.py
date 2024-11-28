09c05cf3f2db21d1a52d331677bf4820eb04ff1c
Geonwoo89 <raccoon09@hanmail.net>
2024-11-28 13:04:11 +0900

    Add files via upload

--- qplus.py
+++ qplus.py
@@ -2,7 +2,7 @@ import os
 import tempfile
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_community.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
